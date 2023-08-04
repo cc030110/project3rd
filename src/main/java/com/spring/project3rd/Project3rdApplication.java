4f6b7ec230b696f022e3d0fd16325417824dbf1b
subin <cc030110@gmail.com>
2023-08-04 18:03:15 +0900

    Initial commit

---
+++ src/main/java/com/spring/project3rd/Project3rdApplication.java
@@ -0,0 +1,13 @@
+package com.spring.project3rd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3rdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3rdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/project3rd/Project3rdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.project3rd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3rdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
