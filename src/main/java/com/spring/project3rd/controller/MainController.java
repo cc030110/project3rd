807ada79dc38cdc553ccdb5056e729d6d2003c9e
subin <cc030110@gmail.com>
2023-08-08 10:20:37 +0900

    update webapp

---
+++ src/main/java/com/spring/project3rd/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.spring.project3rd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
