b8675796f97cf4d48c5ccc9b38c1ad58c6bd9d97
subin <cc030110@gmail.com>
2023-08-09 09:38:21 +0900

    made board tables

---
+++ src/main/java/com/spring/project3rd/domain/boardFree/BoardFree.java
@@ -0,0 +1,4 @@
+package com.spring.project3rd.domain.boardFree;
+
+public class BoardFree {
+}
---
+++ src/main/java/com/spring/project3rd/domain/boardProfile/BoardProfile.java
@@ -0,0 +1,5 @@
+package com.spring.project3rd.domain.boardProfile;
+
+public class BoardProfile {
+
+}
