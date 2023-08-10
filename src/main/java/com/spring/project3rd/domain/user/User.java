4806947dc9c95af4ef465374c21ec9cd5301df0b
subin <cc030110@gmail.com>
2023-08-10 11:49:59 +0900

    merge

--- src/main/java/com/spring/project3rd/domain/user/User.java
+++ src/main/java/com/spring/project3rd/domain/user/User.java
@@ -6,8 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
-import java.math.BigInteger;
-import java.security.Timestamp;
+
 
 @NoArgsConstructor
 @AllArgsConstructor
