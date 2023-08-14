51938ec3e8a088292bfab67a9ffac7a48ec05bff
aquinas <hik130423@gmail.com>
2023-08-14 09:57:28 +0900

    js error catching

--- src/main/resources/static/script/board-community.js
+++ src/main/resources/static/script/board-community.js
@@ -1,16 +1,16 @@
 // 파일처리 부분 추후수정 필요
 
-// $(document).ready(function() {
-//     $('#file').on('change', function() {
-//         let files = $('#file').prop('files'); // FileList 객체
-//         let max = 3; // 최대 선택 파일 개수
-//
-//         if (files.length > max) {
-//             alert('최대 ' + max + '개까지 선택할 수 있습니다.');
-//             $(this).val(''); // 선택한 파일 초기화
-//         }
-//     });
-// });
+$(document).ready(function() {
+    $('#file').on('change', function() {
+        let files = $('#file').prop('files'); // FileList 객체
+        let max = 3; // 최대 선택 파일 개수
+
+        if (files.length > max) {
+            alert('최대 ' + max + '개까지 선택할 수 있습니다.');
+            $(this).val(''); // 선택한 파일 초기화
+        }
+    });
+});
 
 function write(){
     const platform = $('#platform').val();
@@ -24,7 +24,7 @@ function write(){
         alert("제목을 입력해주세요.");
     }else if(contents===""){
         alert("내용을 입력해주세요.");
-    }else if(participantsNum===""){
+    }else if(participants===""){
         alert("인원 수를 입력해주세요.");
     }else{
         let write={
@@ -45,9 +45,9 @@ function write(){
             async:false
         }).done(function (response){
             if(response===null){
-                alert("등록 실패")
+                alert("게시글이 등록되었습니다.")
             }else{
-                alert("등록 성공");
+                alert("게시글 등록에 실패했습니다.");
                 window.location.href="/board/community/"+response.boardNo;
             }
 
