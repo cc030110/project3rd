97d09caa81a403b63cf226f9556354cca02dd542
yhh <yhh6251@gmail.com>
2023-08-14 09:34:52 +0900

    update Jwt2

--- src/main/java/com/spring/project3rd/domain/user/UserRepository.java
+++ src/main/java/com/spring/project3rd/domain/user/UserRepository.java
@@ -1,9 +1,11 @@
 package com.spring.project3rd.domain.user;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
 //    public User findById(String id);
@@ -13,6 +15,8 @@ public interface UserRepository extends JpaRepository<User, String> {
     /** 프로필 게시판 페이징**/
     public List<User> findAllByIdLike(String pattern, Pageable pageable);
 
+    @EntityGraph(attributePaths ="authorities")
+    Optional<User> findOneWithAuthoritiesById(String id);
 
 }
 
---
+++ src/main/java/com/spring/project3rd/security/SecurityService.java
@@ -0,0 +1,43 @@
+package com.spring.project3rd.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class SecurityService {
+    private static final String SECRET_KEY = "gkjsbkgjdfkgjhdkjfhckncghgerighmcigsdfgsdfgo";
+
+    // 로그인 서비스 던질때 같이
+    public String createToken(String subject, long expTime) {
+        if (expTime <= 0) {
+            throw new RuntimeException("만료시간이 0보다 커야함~");
+        }
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        byte[] secretKeyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        Key signingkey = new SecretKeySpec(secretKeyBytes, signatureAlgorithm.getJcaName());
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .signWith(signingkey, signatureAlgorithm)
+                .setExpiration(new Date(System.currentTimeMillis() * expTime))
+                .compact();
+    }
+
+    // 토큰 검증하는 메서드를 만들어서 boolean
+    public String getSubject(String token){
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+}
