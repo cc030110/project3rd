447a04512f15ed47e1c89b2c1d7d1c57bed2b322
yhh <yhh6251@gmail.com>
2023-08-14 14:36:10 +0900

    update

--- src/main/java/com/spring/project3rd/config/SecurityConfig.java
+++ src/main/java/com/spring/project3rd/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.spring.project3rd.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -16,5 +19,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**").permitAll() // 모든 요청에 대해 인증을 허용
                 .and()
                 .csrf().disable(); // CSRF 보안 비활성화 (테스트 환경에서만 사용)
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
