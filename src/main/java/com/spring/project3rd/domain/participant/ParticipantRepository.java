44566905312ca7b903a6348ad2ed9fca6050f3ab
aquinas <hik130423@gmail.com>
2023-08-23 21:42:52 +0900

    Participate working

--- src/main/java/com/spring/project3rd/domain/participant/ParticipantRepository.java
+++ src/main/java/com/spring/project3rd/domain/participant/ParticipantRepository.java
@@ -1,11 +1,15 @@
 package com.spring.project3rd.domain.participant;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.spring.project3rd.domain.platform.Platform;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
-public interface ParticipantRepository {
+public interface ParticipantRepository extends JpaRepository<Platform,String> {
+    // user id를 이용하여 participant id 조회
+    public List<String> findParticipantIdByUserId(String userId);
+
+
 
 }
